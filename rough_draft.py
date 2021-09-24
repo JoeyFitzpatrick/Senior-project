342c84747fd823d0e2a3e45fa131f0aa33af6142
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2021-09-24 18:32:58 -0500

    just getting started

---
+++ rough_draft.py
@@ -0,0 +1,12 @@
+"""
+TODOS:
+Make model of a single IEEE 802.22 network
+Make channels within that network
+Make signals within that network
+Be able to choose the number of channels and signals, then run simulation
+This simulation should then show what signals will use what channels
+How to implement:
+Make network class
+Make channel class
+Make signal class
+"""
